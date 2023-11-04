6f1a88705e0f70be6478cc466c7c03af215e351e
aresrc <117935165+aresrc@users.noreply.github.com>
2023-11-04 11:05:34 -0500

    Cambiazo radical

--- src/ProgramaPrincipal/Main.java
+++ src/ProgramaPrincipal/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("HolaMundo");
         //cambio en el proyecto de POO
         System.out.println("Testeo de GitHub");
+        System.out.println("Cambiazo BC");
     }
     
     
