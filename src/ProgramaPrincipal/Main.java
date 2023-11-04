e1bb582280875352dbb52f89477e8f25919f8061
Andrés Ordóñez <Andrs Ordez@DESKTOP-4DLJKV2>
2023-11-04 12:01:14 -0500

    Nuevo comienzo de nuevo

--- src/ProgramaPrincipal/Main.java
+++ src/ProgramaPrincipal/Main.java
@@ -7,9 +7,6 @@ public class Main {
         CrearArchivo("archivo\\InformacionPacientes.txt");
         EscribirArchivo("archivo\\InformacionPacientes.txt","hola");
         System.out.println("HolaMundo");
-        //cambio en el proyecto de POO
-        System.out.println("Testeo de GitHub");
-        System.out.println("Cambiazo BC");
     }
     
     
