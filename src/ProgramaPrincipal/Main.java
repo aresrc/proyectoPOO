6e801e5c8d55bf319350b8c9ca1c5c8a81c5dc81
admin <admin@DESKTOP-3RLD34G>
2023-11-04 14:36:42 -0500

    Nueva linea de codigo

--- src/ProgramaPrincipal/Main.java
+++ src/ProgramaPrincipal/Main.java
@@ -10,5 +10,7 @@ public class Main {
         //Vamos a realizr una modificación en el codigo
         System.out.println("Lo cambie por ser racista"); //Nueva modificacion;
         System.out.println("Hola de nuevo");//no es un cambio nuevo
+        System.out.println("Cambio por Jesus");//el era el racista
+        System.out.println("Hola que tal");
     }
 }
