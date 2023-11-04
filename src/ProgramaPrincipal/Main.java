5f1b0eba1d50345e2b791f8128c7d6fa1abb5934
Andrés Ordóñez <Andrs Ordez@DESKTOP-4DLJKV2>
2023-11-04 12:34:54 -0500

    Final del tutorial

--- src/ProgramaPrincipal/Main.java
+++ src/ProgramaPrincipal/Main.java
@@ -8,7 +8,7 @@ public class Main {
         EscribirArchivo("archivo\\InformacionPacientes.txt","hola");
         System.out.println("HolaMundo");
         //Vamos a realizr una modificación en el codigo
-        System.out.println("Lo cambie por ser racista") //Nueva modificacion;
+        System.out.println("Lo cambie por ser racista"); //Nueva modificacion;
     }
     
     
