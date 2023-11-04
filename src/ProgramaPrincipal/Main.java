98e03a465afcce133248b9c613c8dda60e97eb9d
Andrés Ordóñez <Andrs Ordez@DESKTOP-4DLJKV2>
2023-11-04 10:38:20 -0500

    amend commit

--- src/ProgramaPrincipal/Main.java
+++ src/ProgramaPrincipal/Main.java
@@ -7,8 +7,6 @@ public class Main {
         CrearArchivo("archivo\\InformacionPacientes.txt");
         EscribirArchivo("archivo\\InformacionPacientes.txt","hola");
         System.out.println("HolaMundo");
-        System.out.println("NuevoCommit");
-        System.out.println("Nuevas Ramas");
     }
     
     
