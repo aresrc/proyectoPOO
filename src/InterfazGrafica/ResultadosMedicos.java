680693c339c0afcbfa05d02fc05886065a140a09
admin <admin@DESKTOP-3RLD34G>
2023-11-07 13:16:15 -0500

    reestruc

---
+++ src/InterfazGrafica/ResultadosMedicos.java
@@ -0,0 +1,125 @@
+
+package InterfazGrafica;
+
+import java.awt.CardLayout;
+
+
+public class ResultadosMedicos extends javax.swing.JFrame {
+
+    public ResultadosMedicos() {
+        initComponents();
+        
+        ResultCardiologia rcard = new ResultCardiologia();
+        ResultNeumologia neum = new ResultNeumologia();
+        ResultOftalmologia oft = new ResultOftalmologia();
+        
+        
+        contenido.setSize(430,540);
+        CardLayout cardLayout = new CardLayout();
+        contenido.setLayout(cardLayout);
+        contenido.add(rcard,"panel1");
+        contenido.add(neum,"panel2");
+        contenido.add(oft,"panel3");
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        contenido = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        btnatras = new javax.swing.JButton();
+        btnsiguiente = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        contenido.setBackground(new java.awt.Color(255, 255, 255));
+        contenido.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(contenido, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 200, 500, 540));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("Resultados obtenidos");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 20, -1, -1));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 204));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnatras.setText("ATRAS");
+        btnatras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnatrasActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnatras, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 240, 70));
+
+        btnsiguiente.setText("SIGUIENTE");
+        btnsiguiente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnsiguienteActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnsiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 0, 260, 70));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 740, 500, 70));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnsiguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnsiguienteActionPerformed
+        CardLayout cardLayout = (CardLayout) contenido.getLayout();
+        cardLayout.next(contenido); // Muestra el siguiente panel
+        contenido.setSize(430,540);
+    }//GEN-LAST:event_btnsiguienteActionPerformed
+
+    private void btnatrasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnatrasActionPerformed
+        CardLayout cardLayout = (CardLayout) contenido.getLayout();
+        cardLayout.previous(contenido); // Muestra el panel anterior
+        contenido.setSize(430,540);
+    }//GEN-LAST:event_btnatrasActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ResultadosMedicos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ResultadosMedicos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ResultadosMedicos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ResultadosMedicos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ResultadosMedicos().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnatras;
+    private javax.swing.JButton btnsiguiente;
+    private javax.swing.JPanel contenido;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
